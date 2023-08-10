68cd526dc480e025602791ff85bbe54e1a69b649
Mostapha139 <mostaphamoujoud1@gmail.com>
2023-08-10 14:35:51 -0700

    moso

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 
 /**
  * main - Entry point
- * Description: A C program that prints woth put function
+ * Description: A C program that prints with put function
  *
- * Return: Always 0 (Success)
- */
+ * Return: Alawys 0 (Success)
+*/
 
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+  	puts("\"Programming is like building a multilingual puzzle");
+  	return (0);
 }
