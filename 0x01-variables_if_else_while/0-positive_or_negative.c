cc9dca35b81bc5c70d353b0e4523eaa294b15262
Mostapha139 <mostaphamoujoud1@gmail.com>
2023-08-11 11:02:35 -0700

    moso

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,22 +5,21 @@
 /**
  * main - Entry point
  *
- * Description: plus or minus
+ * Description: False or true validation
  *
  * Return : 0 (success)
 *\
 
-int main(void)
+int main(main)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0)
-		printf("%i is positive\n",n)
+	if (n > 0)
+		printf("%i is positive\n", n);
 	else if (n == 0)
-		printf("%i is zero\n",n)
+		printf("%i is zero\n", n);
 	else
-		printf("%i is negative\n",n)
+		printf("%i is negative\n", n);
 	return (0);
 }
