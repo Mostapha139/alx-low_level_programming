3c842a9fc860919042bc2f12d82fdf29b8830d6c
Mostapha139 <mostaphamoujoud1@gmail.com>
2023-11-14 23:18:37 -0800

    moso

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,12 +6,12 @@
  * @y: the exponent
  * Return: the result of x^y
  */
-int pow_recursion(int x, int y)
+int _pow_recursion(int x, int y)
 {
     if (y < 0)
         return (-1);
     else if (y == 0)
         return (1);
     else
-        return (x * pow_recursion(x, (y - 1)));
+        return (x * _pow_recursion(x, (y - 1)));
 }
