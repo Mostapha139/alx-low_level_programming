d0c78938c93781dd6d0a13911f8bd0bcb0b74311
Mostapha139 <mostaphamoujoud1@gmail.com>
2023-11-30 11:05:46 -0800

    moso

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
  * sum_them_all - adds all the numbers
@@ -24,3 +25,4 @@ int sum_them_all(const unsigned int n, ...)
 
 	return (sum);
 }
+
